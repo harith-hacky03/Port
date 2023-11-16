81f238b8dd330aa59677b76e35f0ca50fb19d342
Harith <108193091+harith-hacky03@users.noreply.github.com>
2023-11-16 22:02:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Contact from './Components/Contact'
+import Skills from './Components/Skills'
+import Landing from './Components/Landing'
+import About from './Components/About'
+import Projects from './Components/Projects'
+const App = () => {
+
+
+
+  return (
+    <div>
+      <Landing/>
+      <About/>
+      <Skills/>
+      <Projects/>
+      <Contact/>
+    </div>
+  )
+}
+
+export default App
